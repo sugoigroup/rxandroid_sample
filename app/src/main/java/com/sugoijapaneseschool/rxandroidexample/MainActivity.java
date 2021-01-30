23007802b49b21097228e1adec35f667259b9ee5
kimtaeho <>
2021-01-31 07:47:47 +0900

    3. 구독 아이템 동적 추가해서 발행

--- app/src/main/java/com/sugoijapaneseschool/rxandroidexample/MainActivity.java
+++ app/src/main/java/com/sugoijapaneseschool/rxandroidexample/MainActivity.java
@@ -1,64 +1,49 @@
 package com.sugoijapaneseschool.rxandroidexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.concurrent.TimeUnit;
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.observables.ConnectableObservable;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Disposable mDisposable;
-    TextView helloTv;
-    TextView helloTv2;
+    private ConnectableObservable<String> dataStream;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        helloTv = (TextView) findViewById(R.id.hello);
-        helloTv2 = (TextView) findViewById(R.id.hello2);
+        TextView helloTv = (TextView) findViewById(R.id.hello);
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(e -> btnBlicked());
+
+        dataStream = Observable.just("Hello ! from just" )
+                .observeOn(AndroidSchedulers.mainThread())
+                .publish();
+        dataStream.subscribe(s -> helloTv.setText(s));
+
+    }
+
+    public void addMe(View view) {
+        TextView helloTv2 = (TextView) findViewById(R.id.hello2);
+        dataStream.subscribe(s -> helloTv2.setText(s));
     }
 
     private void btnBlicked() {
-
-        //timer 를 이용하면 앞서 했던 thread 를 안써도 일정시간 지난후 구독이 이루어지게 할수 있다..
-        Observable.timer(3, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())  //결과는 화면 메인쓰레드에서 하자.
-                .subscribe(d -> {
-                    changeText(getJest());
-                });
-
-
-        //just 는 아이템을 바로바로 서브스크들한테 방출한다.
-        Observable.just("Hello ! from just").subscribe(s -> changeText(s));
-    }
-
-    private void changeText(String txt) {
-        helloTv.setText(txt);
-        helloTv2.setText(txt);
-    }
-
-    private String getJest() {
-        return "Hello! from create";
+        disposable = dataStream.connect();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        disposable.dispose();
     }
 }
